50a3916c51bc806b9087e80d793401ed24b3a660
diepanhng0711 <diepanhng0711@gmail.com>
2023-01-25 18:06:56 +0700

    Tam thoi ghep phan gio hang thanh toan

---
+++ app/Http/Controllers/CartsController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ItemCart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
+
+class CartsController extends Controller
+{
+    public function Index(){
+        $res = Http::get('https://p01-product-api-production.up.railway.app/api/user/products');
+        return view('cart.home',['product'=> $res['data']]);
+    
+    }
+
+    public function SameProduct(){
+        $res = Http::get('https://p01-product-api-production.up.railway.app/api/user/products');
+        return view('cart.same-product',['product'=> $res['data']]);
+    
+    }
+
+    public function BuyAgain(){
+        $cart = DB::table('item_carts')->where('status',1)->get();
+        return view('cart.buy-again',compact('cart'));   
+    }
+
+    public function AddToCart(Request $req,$id){
+        $res = Http::get('https://p01-product-api-production.up.railway.app/api/user/products');
+        // $id_user = $req->id;
+        $id_user = Auth::user()->id;
+        $cart_item = new ItemCart();
+        foreach($res['data'] as $prd){
+            if($prd['sub_products'] != null){
+                foreach($prd['sub_products'] as $item){
+                    if($item['id'] == $id){
+                        if(ItemCart::where('id_user',$id_user)->where('id_product',$id)->where('status',1)->exists()){
+                            $now_quanty = ItemCart::where('id_user',$id_user)->where('id_product',$id)->where('status',1)->first();
+                            $i = $now_quanty->quanty + 1;
+                            $cost = $prd['cost'] * $i;
+                            ItemCart::where('id_user',$id_user)
+                            ->where('id_product',$id)
+                            ->update(['quanty'=>$i]);
+                            ItemCart::where('id_user',$id_user)
+                            ->where('id_product',$id)
+                            ->update(['total_price'=>$cost]);
+                        }else{
+                            $cart_item->id_user = $id_user;
+                            $cart_item->id_product = $item['id'];
+                            $cart_item->name = $prd['name'];
+                            $cart_item->quanty = 1;
+                            $cart_item->size = $item['size'];
+                            $cart_item->color = $item['color'];
+                            $cart_item->price = $prd['cost'];
+                            $cart_item->total_price = $prd['cost'];
+                            $cart_item->image_url = $item['image_url'];
+                            $cart_item->status = 1;
+
+                            $cart_item->save();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public function ViewToCart(){
+        $cart = DB::table('item_carts')->where('status',1)->get();
+
+        $totalQuanty = DB::table('item_carts')->where('status',1)->sum('quanty');
+        $totalPrice = DB::table('item_carts')->where('status',1)->sum('total_price');
+        return view('cart.cart',compact('cart'),compact('totalQuanty','totalPrice'));
+    }
+
+    public function DeleteItemListToCart($id){
+       if(ItemCart::where('id_product',$id)->exists()){
+        ItemCart::where('id_product',$id)
+        ->update(['status'=>0]);
+       }
+       $cart = DB::table('item_carts')->where('status',1)->get();
+       $totalQuanty = DB::table('item_carts')->where('status',1)->sum('quanty');
+       $totalPrice = DB::table('item_carts')->where('status',1)->sum('total_price');
+       return view('cart.list-cart',compact('cart'),compact('totalQuanty','totalPrice'));
+    }
+
+    public function SaveItemListToCart(Request $req,$id,$quanty){
+        if(ItemCart::where('id_product',$id)->exists()){
+            ItemCart::where('id_product',$id)
+            ->update(['quanty'=>$quanty]);
+            $now_quanty = ItemCart::where('id_product',$id)->where('status',1)->first();
+            $i = $now_quanty->quanty;
+            $cost = $now_quanty->price * $i;
+            ItemCart::where('id_product',$id)
+            ->update(['total_price'=>$cost]);
+        }
+        $cart = DB::table('item_carts')->where('status',1)->get();
+        $totalQuanty = DB::table('item_carts')->where('status',1)->sum('quanty');
+        $totalPrice = DB::table('item_carts')->where('status',1)->sum('total_price');
+        return view('cart.list-cart',compact('cart'),compact('totalQuanty','totalPrice'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -138,3 +138,26 @@ Route::get('/user-info', [
 ]);
 
 
+//CART - CHECKOUT - INVOICE
+Route::get('/cart-home', 'App\Http\Controllers\CartsController@Index');
+Route::get('/AddtoCart/{id}', 'App\Http\Controllers\CartsController@AddToCart');
+Route::get('/Delete-Item-Cart/{id}', 'App\Http\Controllers\CartsController@DeleteItemToCart');
+Route::get('/Cart', 'App\Http\Controllers\CartsController@ViewtoCart');
+Route::get('/same-product', 'App\Http\Controllers\CartsController@SameProduct');
+Route::get('/buy-again', 'App\Http\Controllers\CartsController@BuyAgain');
+Route::get('/Delete-Item-List-Cart/{id}', 'App\Http\Controllers\CartsController@DeleteItemListToCart');
+Route::get('/Save-Item-List-Cart/{id}/{quanty}', 'App\Http\Controllers\CartsController@SaveItemListToCart');
+Route::get('/Update-Item-List-Cart/{id}/{quanty}', 'App\Http\Controllers\CartController@UpdateItemListCart');
+
+//API
+Route::get('/Api/Product-Cart', 'App\Http\Controllers\CartController@product_cart');
+Route::get('/Api/totalQuanty-Product-Cart', 'App\Http\Controllers\CartController@total_product_cart');
+
+//Invoice
+Route::get('/create-invoice','App\Http\Controllers\InvoiceController@Invoice');
+Route::get('/payment','App\Http\Controllers\InvoiceController@SaveInvoice');
+
+//Payment
+Route::get('/Sucess-payment','App\Http\Controllers\PaymentController@DonePayment');
+Route::post('/VN-pay-payment','App\Http\Controllers\PaymentController@VnpayPayment');
+Route::get('/Paypal-payment','App\Http\Controllers\PaymentController@PaypalPayment');
