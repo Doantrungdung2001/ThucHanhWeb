ea62bef3d9a81f051da853fdc98246b90f6a5fff
thahduog <duong.nt194531@sis.hust.edu.vn>
2023-01-26 23:51:11 +0700

    back phan đăng ký có đủ thông tin

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -24,11 +24,20 @@ class CreateNewUser implements CreatesNewUsers
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+
+            'ho_va_ten' => ['required', 'string', 'max:255'],
+            'sdt' => ['required', 'numeric','digits:10'],
+            'dia_chi' => ['required', 'string', 'max:255'],
+            'ngay_sinh' => ['required','date','date_format:Y-m-d','before:'.now()->subYears(18)->toDateString()],
+            
         ])->validate();
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'ho_va_ten' => $input['ho_va_ten'],
+            'dia_chi' => $input['dia_chi'],
+            'ngay_sinh' => $input['ngay_sinh'],
             'password' => Hash::make($input['password']),
         ]);
     }
