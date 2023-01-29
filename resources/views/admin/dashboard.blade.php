015b99e7e47c0515cbcb551d7178deeb2e39c843
diepanhng0711 <diepanhng0711@gmail.com>
2023-01-29 10:07:52 +0700

    Them header chao mung trang quan tri

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('admin_layout')
 @section('admin_content')
-<h3>Welcome to Admin page</h3>
+<h3>Chào mừng @php print(Auth::user()->name); @endphp đến với trang quản trị</h3>
 <img src="https://lh3.googleusercontent.com/bOhXrUBojZE3bnjToKeYpnmCOBhyMsVaPeHQX2NaFG_M220Kbu7NraVHJWsaFm8KM67QaI7l3tjotki75s3bt3Zixbk04D4pHqpe1f7_k1rpKNujIfZk6buhxAudcqCvg6bHlFMEYQ=w2400">
 @endsection
