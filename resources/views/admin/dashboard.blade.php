3dc7a85d6b32e25c58b6d340f23a1caf916994af
AnhNguyen208 <nguyenletuananh20082001@gmail.com>
2023-01-31 16:12:08 +0700

    update dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -27,9 +27,16 @@
                     caretPadding: 10,
                 },
                 legend: {
-                    display: false
+                    position: 'left',
+                    display: true
+
                 },
                 cutoutPercentage: 80,
+                title: {
+                    position: 'bottom',
+                    display: true,
+                    text: 'Người dùng'
+                },
             },
         });
 
@@ -58,9 +65,15 @@
                     caretPadding: 10,
                 },
                 legend: {
-                    display: false
+                    position: 'left',
+                    display: true
                 },
                 cutoutPercentage: 80,
+                title: {
+                    position: 'bottom',
+                    display: true,
+                    text: 'Sản phẩm'
+                },
             },
         });
     </script>
@@ -102,7 +115,7 @@
                         </li>
                         <li class="d-flex mb-4 pb-1">
                             <div class="avatar flex-shrink-0 me-3">
-                                <span class="avatar-initial rounded bg-label-success"><i class="bx bx-closet"></i></span>
+                                <span class="avatar-initial rounded bg-label-success"><i class='bx bxs-t-shirt'></i></span>
                             </div>
                             <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                 <div class="me-2">
@@ -147,102 +160,56 @@
                 </div>
             </div>
         </div>
+
         <div class="col-md-6 col-lg-4 order-1 mb-4">
             <div class="card h-100">
-                <div class="card-header">
-                    <ul class="nav nav-pills" role="tablist">
-                        <li class="nav-item">
-                            <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab"
-                                data-bs-target="#navs-tabs-line-card-income" aria-controls="navs-tabs-line-card-income"
-                                aria-selected="true">
-                                Income
-                            </button>
-                        </li>
-                        <li class="nav-item">
-                            <button type="button" class="nav-link" role="tab">Expenses</button>
-                        </li>
-                        <li class="nav-item">
-                            <button type="button" class="nav-link" role="tab">Profit</button>
-                        </li>
-                    </ul>
-                </div>
-                <div class="card-body px-0">
-                    <div class="tab-content p-0">
-                        <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
-                            <div class="d-flex p-4 pt-3">
-                                <div class="avatar flex-shrink-0 me-3">
-                                    <img src="../assets/img/icons/unicons/wallet.png" alt="User" />
-                                </div>
-                                <div>
-                                    <small class="text-muted d-block">Total Balance</small>
-                                    <div class="d-flex align-items-center">
-                                        <h6 class="mb-0 me-1">$459.10</h6>
-                                        <small class="text-success fw-semibold">
-                                            <i class="bx bx-chevron-up"></i>
-                                            42.9%
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                            <div id="incomeChart"></div>
-                            <div class="d-flex justify-content-center pt-4 gap-2">
-                                <div class="flex-shrink-0">
-                                    <div id="expensesOfWeek"></div>
-                                </div>
-                                <div>
-                                    <p class="mb-n1 mt-1">Expenses This Week</p>
-                                    <small class="text-muted">$39 less than last week</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-4 order-2 mb-4">
-            <div class="card h-100">
                 <div class="card-header d-flex align-items-center justify-content-between">
-                    <h5 class="card-title m-0 me-2">Một số thống kê khác</h5>
+                    <h5 class="card-title m-0 me-2">Thống kê người dùng và sản phẩm</h5>
                 </div>
                 <div class="card-body">
                     <ul class="p-0 m-0">
                         <li class="d-flex mb-4 pb-1">
-                            <div class="avatar flex-shrink-0 me-3">
-                                <img src="../assets/img/icons/unicons/paypal.png" alt="User" class="rounded" />
-                            </div>
-                            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                                <div class="me-2">
-                                    {{-- <small class="text-muted d-block mb-1">Paypal</small> --}}
-                                    <h6 class="mb-0">Số người dùng</h6>
-                                </div>
-                                <div class="user-progress d-flex align-items-center gap-1">
-                                    <h6 class="mb-0">{{ $total[0] }}</h6>
-                                    <span class="text-muted">người</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="d-flex mb-4 pb-1">
                             <canvas id="myPieChart" style="width:100%;max-width:700px"></canvas>
-                        </li>
-                        <li class="d-flex mb-4 pb-1">
-                            <div class="avatar flex-shrink-0 me-3">
-                                <img src="../assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
-                            </div>
-                            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                                <div class="me-2">
-                                    {{-- <small class="text-muted d-block mb-1">Wallet</small> --}}
-                                    <h6 class="mb-0">Số sản phẩm</h6>
-                                </div>
-                                <div class="user-progress d-flex align-items-center gap-1">
-                                    <h6 class="mb-0">{{ $total[1] }}</h6>
-                                    <span class="text-muted">sản phẩm</span>
-                                </div>
-                            </div>
                         </li>
                         <li class="d-flex mb-4 pb-1">
                             <canvas id="myPieChart1" style="width:100%;max-width:700px"></canvas>
                         </li>
                     </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-4 order-2 mb-4">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-start justify-content-between">
+                                <div class="avatar flex-shrink-0">
+                                    <span class="avatar-initial rounded bg-label-success">
+                                        <i class='bx bx-user' style='color:#26ce33'></i>
+                                    </span>
+                                </div>
+                            </div>
+
+                            <span class="fw-semibold d-block mb-1">Tổng số người dùng</span>
+                            <h3 class="card-title mb-2">{{ $total[0] }}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-start justify-content-between">
+                                <div class="avatar flex-shrink-0">
+                                    <span class="avatar-initial rounded bg-label-primary">
+                                        <i class='bx bxl-product-hunt'></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <span>Tống số sản phẩm</span>
+                            <h3 class="card-title text-nowrap mb-1">{{ $total[1] }}</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
